8208226ee9999448ab6708741ea41517809667ef
thdihan <thdihan@outlook.com>
2024-10-12 11:59:39 +0600

    Category fetch done

--- src/components/UI/CreatePost.tsx
+++ src/components/UI/CreatePost.tsx
@@ -2,7 +2,6 @@
 
 import { Button } from "@nextui-org/button";
 import { Checkbox } from "@nextui-org/checkbox";
-import { Link } from "@nextui-org/link";
 import {
     Modal,
     ModalBody,
@@ -13,13 +12,9 @@ import {
 import { ChangeEvent, useRef, useState } from "react";
 import JoditEditor from "jodit-react";
 import { Select, SelectItem } from "@nextui-org/select";
-import { FieldValues, SubmitHandler } from "react-hook-form";
 
-const categoryOption = [
-    { label: "Category 1", key: "category1" },
-    { label: "Category 2", key: "category2" },
-    { label: "Category 3", key: "category3" },
-];
+import { useGetCategories } from "@/src/hooks/categories.hook";
+import LoadingSpinner from "./Loading";
 
 export default function CreatePostModal({
     isOpen,
@@ -36,6 +31,25 @@ export default function CreatePostModal({
     const [isPremium, setIsPremium] = useState<boolean>(false);
     const editor = useRef(null);
     const [content, setContent] = useState("");
+
+    const {
+        data: categoryData,
+        isSuccess: categorySuccess,
+        isPending: getCategoryPending,
+    } = useGetCategories();
+
+    let categoryOption: { key: string; label: string }[] = [];
+
+    if (categoryData?.data && categorySuccess) {
+        categoryOption = categoryData?.data.map(
+            (category: { _id: string; name: string }) => {
+                return {
+                    key: category._id,
+                    label: category.name,
+                };
+            }
+        );
+    }
 
     const onSubmit = () => {
         const formData = new FormData();
@@ -75,117 +89,122 @@ export default function CreatePostModal({
     };
 
     return (
-        <Modal
-            isOpen={isOpen}
-            placement="top-center"
-            size="5xl"
-            onOpenChange={onOpenChange}
-        >
-            <ModalContent>
-                {(onClose) => (
-                    <>
-                        <ModalHeader className="flex flex-col gap-1">
-                            Create Post
-                        </ModalHeader>
-                        <ModalBody>
-                            <JoditEditor
-                                ref={editor}
-                                value={content}
-                                // tabIndex={1} // tabIndex of textarea
-                                onBlur={(newContent) => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
-                                onChange={(newContent) => {}}
-                            />
-                            {imagePreviews.length > 0 && (
-                                <div className="flex gap-5 my-5 flex-wrap">
-                                    {imagePreviews.map((imageDataUrl) => (
-                                        <div
-                                            key={imageDataUrl}
-                                            className="relative size-48 rounded-xl border-2 border-dashed border-default-300 p-2"
-                                        >
-                                            <img
-                                                alt="item"
-                                                className="h-full w-full object-cover object-center rounded-md"
-                                                src={imageDataUrl}
-                                            />
-                                        </div>
-                                    ))}
-                                </div>
-                            )}
-                            <div className="min-w-fit flex-1">
-                                <label
-                                    className="flex h-14 w-full cursor-pointer items-center justify-center rounded-xl border-2 border-default-200 text-default-500 shadow-sm transition-all duration-100 hover:border-default-400"
-                                    htmlFor="image"
-                                >
-                                    Upload image
-                                </label>
-                                <input
-                                    multiple
-                                    className="hidden"
-                                    id="image"
-                                    type="file"
-                                    onChange={(e) => handleImageChange(e)}
+        <>
+            {getCategoryPending && <LoadingSpinner />}
+            <Modal
+                isOpen={isOpen}
+                placement="top-center"
+                size="5xl"
+                onOpenChange={onOpenChange}
+            >
+                <ModalContent>
+                    {(onClose) => (
+                        <>
+                            <ModalHeader className="flex flex-col gap-1">
+                                Create Post
+                            </ModalHeader>
+                            <ModalBody>
+                                <JoditEditor
+                                    ref={editor}
+                                    value={content}
+                                    // tabIndex={1} // tabIndex of textarea
+                                    onBlur={(newContent) =>
+                                        setContent(newContent)
+                                    } // preferred to use only this option to update the content for performance reasons
+                                    onChange={(newContent) => {}}
                                 />
-                            </div>
+                                {imagePreviews.length > 0 && (
+                                    <div className="flex gap-5 my-5 flex-wrap">
+                                        {imagePreviews.map((imageDataUrl) => (
+                                            <div
+                                                key={imageDataUrl}
+                                                className="relative size-48 rounded-xl border-2 border-dashed border-default-300 p-2"
+                                            >
+                                                <img
+                                                    alt="item"
+                                                    className="h-full w-full object-cover object-center rounded-md"
+                                                    src={imageDataUrl}
+                                                />
+                                            </div>
+                                        ))}
+                                    </div>
+                                )}
+                                <div className="min-w-fit flex-1">
+                                    <label
+                                        className="flex h-14 w-full cursor-pointer items-center justify-center rounded-xl border-2 border-default-200 text-default-500 shadow-sm transition-all duration-100 hover:border-default-400"
+                                        htmlFor="image"
+                                    >
+                                        Upload image
+                                    </label>
+                                    <input
+                                        multiple
+                                        className="hidden"
+                                        id="image"
+                                        type="file"
+                                        onChange={(e) => handleImageChange(e)}
+                                    />
+                                </div>
 
-                            <div className="min-w-fit flex-1">
-                                {/* <FXSelect
+                                <div className="min-w-fit flex-1">
+                                    {/* <FXSelect
                                     // disabled={!categorySuccess}
                                     label="Category"
                                     name="category"
                                     options={categoryOption}
                                 /> */}
-                                <Select
-                                    className="min-w-full sm:min-w-[225px]"
-                                    // isDisabled={disabled}
-                                    label="Category"
-                                    value={category}
-                                    onChange={(e) =>
-                                        setCategory(e.target.value)
-                                    }
-                                >
-                                    {categoryOption.map((option) => (
-                                        <SelectItem key={option.key}>
-                                            {option.label}
-                                        </SelectItem>
-                                    ))}
-                                </Select>
-                            </div>
+                                    <Select
+                                        className="min-w-full sm:min-w-[225px]"
+                                        // isDisabled={disabled}
+                                        label="Category"
+                                        value={category}
+                                        onChange={(e) =>
+                                            setCategory(e.target.value)
+                                        }
+                                    >
+                                        {categoryOption.map((option) => (
+                                            <SelectItem key={option.key}>
+                                                {option.label}
+                                            </SelectItem>
+                                        ))}
+                                    </Select>
+                                </div>
 
-                            <div className="flex py-2 px-1 justify-between">
-                                <Checkbox
-                                    checked={isPremium}
-                                    classNames={{
-                                        label: "text-small",
-                                    }}
-                                    onChange={(e) =>
-                                        setIsPremium(e.target.checked)
-                                    }
+                                <div className="flex py-2 px-1 justify-between">
+                                    <Checkbox
+                                        checked={isPremium}
+                                        classNames={{
+                                            label: "text-small",
+                                        }}
+                                        onChange={(e) =>
+                                            setIsPremium(e.target.checked)
+                                        }
+                                    >
+                                        Premium
+                                    </Checkbox>
+                                </div>
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button
+                                    color="danger"
+                                    variant="flat"
+                                    onPress={onClose}
                                 >
-                                    Premium
-                                </Checkbox>
-                            </div>
-                        </ModalBody>
-                        <ModalFooter>
-                            <Button
-                                color="danger"
-                                variant="flat"
-                                onPress={onClose}
-                            >
-                                Close
-                            </Button>
-                            <Button
-                                color="primary"
-                                onPress={() => {
-                                    onSubmit();
-                                    onClose();
-                                }}
-                            >
-                                Sign in
-                            </Button>
-                        </ModalFooter>
-                    </>
-                )}
-            </ModalContent>
-        </Modal>
+                                    Close
+                                </Button>
+                                <Button
+                                    color="primary"
+                                    onPress={() => {
+                                        onSubmit();
+                                        onClose();
+                                    }}
+                                >
+                                    Sign in
+                                </Button>
+                            </ModalFooter>
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
+        </>
     );
 }
--- src/services/Categories/index.ts
+++ src/services/Categories/index.ts
@@ -3,7 +3,7 @@ import { axiosInstance } from "@/src/lib/AxiosInstance";
 
 export const getCategories = async () => {
     try {
-        const { data } = await axiosInstance.get("/item-categories");
+        const { data } = await axiosInstance.get("/category");
 
         return data;
     } catch (error: any) {
